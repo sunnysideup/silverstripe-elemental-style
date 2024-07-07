9339d60318e2d52074c6f38322da878d3c8b70da
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-07-08 11:52:00 +1200

    FIX: fixing properties

--- src/Model/StyleObject.php
+++ src/Model/StyleObject.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Jellygnite\ElementalStyle\Model;
 
 use SilverStripe\Forms\FormField;
 use SilverStripe\Dev\Debug;
+
 /**
  * Class StyleObject.
  *
@@ -22,55 +24,55 @@ use SilverStripe\Dev\Debug;
  * @method array getStyles()
  *
  */
- 
-class StyleObject implements \JsonSerializable {
-	
-	
-	protected $index;
-	protected $title;
-	protected $description;
-	protected $tab;
-	protected $location;
-	protected $after;	// insert field after
-	protected $styles;
-	protected $options; // use this to create other form fields
-	protected $prefix;
-	protected $suffix;
-	protected $default;
-	protected $group;
-	
-	
-	private static $arr_default = [
-		'Title' => null,
-		'Description' => null,
-		'Tab' => null,
-		'Location' => null,
-		'After' => null,
-		'Styles' => [],		
-		'Sort' => 100,
-		'Options' => [],	
-		'Prefix' => null,
-		'Suffix' => null,
-		'Default' => null,	// this gets applied when nothing is selected
-		'Group' => null,
-	];
 
-	private static $arr_default_options = [
-		'Type' => null,
-		'ShowLabels' => null,
-		'Min' => null,
-		'Max' => null,
-		'Unit' => null,
-		'Step' => null,
-		'ImageSize' => null
-	];
+class StyleObject implements \JsonSerializable
+{
+    protected $index;
+    protected $title;
+    protected $description;
+    protected $tab;
+    protected $location;
+    protected $after;	// insert field after
+    protected $styles;
+    protected $options; // use this to create other form fields
+    protected $prefix;
+    protected $suffix;
+    protected $default;
+    protected $group;
+    protected $sort;
 
-    public function __construct(String $index, Array $arr_object = [])
+
+    private static $arr_default = [
+        'Title' => null,
+        'Description' => null,
+        'Tab' => null,
+        'Location' => null,
+        'After' => null,
+        'Styles' => [],
+        'Sort' => 100,
+        'Options' => [],
+        'Prefix' => null,
+        'Suffix' => null,
+        'Default' => null,	// this gets applied when nothing is selected
+        'Group' => null,
+    ];
+
+    private static $arr_default_options = [
+        'Type' => null,
+        'ShowLabels' => null,
+        'Min' => null,
+        'Max' => null,
+        'Unit' => null,
+        'Step' => null,
+        'ImageSize' => null
+    ];
+
+    public function __construct(String $index, array $arr_object = [])
     {
-		
-		$arr_style = array_merge(self::$arr_default, $arr_object);
-		
-		
+
+        $arr_style = array_merge(self::$arr_default, $arr_object);
+
+
         $this->index = $index;
         $this->title = $arr_style['Title']; //(!empty($arr_style['Title'])) ? $arr_style['Title'] : FormField::name_to_label($this->index);
         $this->description = $arr_style['Description'];
@@ -99,96 +101,113 @@ class StyleObject implements \JsonSerializable {
         );
     }
 
-	public function getIndex(){
-		return $this->index;
-	}
-	public function getTitle(){
-		return (!empty($this->title)) ?  $this->title : FormField::name_to_label($this->index);
-	}
-	public function getDescription(){
-		return $this->description;
-	}
-	public function getTab(){
-		return $this->tab;
-	}
-	public function getLocation(){
-		return $this->location;
-	}
-	public function getAfter(){
-		return $this->after;
-	}
-	// can pass in array or single value
-	public function getStyles(){
-		$arrReturn = [];
-		$styles = $this->styles;
-		foreach($styles as $key => $value){
-			if(is_array($value) && array_key_exists('Value',$value) ){
-				$arrReturn[$key] = $value['Value'];
-			} else {
-				$arrReturn[$key] = $value;
-			}
-		}
-		return $arrReturn;
-	}
-	public function getImages(){
-		$arrReturn = [];
-		$styles = $this->styles;
-		foreach($styles as $key => $value){
-			if(is_array($value) && array_key_exists('Image',$value) ){
-				$arrReturn[$key] = $value['Image'];
-			} else {
-				$arrReturn[$key] = null;
-			}
-		}
-		return $arrReturn;
-	}
-	public function getOptions(){
-		return $this->options;
-	}
-	public function getSort(){
-		return $this->sort;
-	}
-	public function getPrefix(){
-		return $this->prefix;
-	}
-	public function getSuffix(){
-		return $this->suffix;
-	}
-	public function getType(){
-		if($options = $this->getOptions()){
-			return $options['Type'];
-		}
-		return null;
-	}
-	public function getDefault(){
-		return $this->default;
-	}
-	public function getGroup(){
-		return $this->group;
-	}
-	
-	// return selected value
-	public function getSelected(){
-		$styles = $this->getStyles();
-		if(is_array($styles) && array_key_exists('Selected', $styles)){
-			return $styles['Selected'];
-		}
-		return null;
-	}
+    public function getIndex()
+    {
+        return $this->index;
+    }
+    public function getTitle()
+    {
+        return (!empty($this->title)) ? $this->title : FormField::name_to_label($this->index);
+    }
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    public function getTab()
+    {
+        return $this->tab;
+    }
+    public function getLocation()
+    {
+        return $this->location;
+    }
+    public function getAfter()
+    {
+        return $this->after;
+    }
+    // can pass in array or single value
+    public function getStyles()
+    {
+        $arrReturn = [];
+        $styles = $this->styles;
+        foreach($styles as $key => $value) {
+            if(is_array($value) && array_key_exists('Value', $value)) {
+                $arrReturn[$key] = $value['Value'];
+            } else {
+                $arrReturn[$key] = $value;
+            }
+        }
+        return $arrReturn;
+    }
+    public function getImages()
+    {
+        $arrReturn = [];
+        $styles = $this->styles;
+        foreach($styles as $key => $value) {
+            if(is_array($value) && array_key_exists('Image', $value)) {
+                $arrReturn[$key] = $value['Image'];
+            } else {
+                $arrReturn[$key] = null;
+            }
+        }
+        return $arrReturn;
+    }
+    public function getOptions()
+    {
+        return $this->options;
+    }
+    public function getSort()
+    {
+        return $this->sort;
+    }
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }
+    public function getSuffix()
+    {
+        return $this->suffix;
+    }
+    public function getType()
+    {
+        if($options = $this->getOptions()) {
+            return $options['Type'];
+        }
+        return null;
+    }
+    public function getDefault()
+    {
+        return $this->default;
+    }
+    public function getGroup()
+    {
+        return $this->group;
+    }
 
-	// return selected value nicely formatted
-	public function getFormattedSelected(){
-		$selected = $this->getSelected();
-		if(is_array($selected)){
-			$output = '';
-			foreach($selected AS $item){
-				$output .= $this->getPrefix() . $item . $this->getSuffix() . ' ';
-			}
-			return trim($output);
-		} elseif(!empty($selected) || $selected==='0'){
-			return $this->getPrefix() . $selected . $this->getSuffix();			
-		}
-		return $this->getDefault();
-	}
+    // return selected value
+    public function getSelected()
+    {
+        $styles = $this->getStyles();
+        if(is_array($styles) && array_key_exists('Selected', $styles)) {
+            return $styles['Selected'];
+        }
+        return null;
+    }
+
+    // return selected value nicely formatted
+    public function getFormattedSelected()
+    {
+        $selected = $this->getSelected();
+        if(is_array($selected)) {
+            $output = '';
+            foreach($selected as $item) {
+                $output .= $this->getPrefix() . $item . $this->getSuffix() . ' ';
+            }
+            return trim($output);
+        } elseif(!empty($selected) || $selected === '0') {
+            return $this->getPrefix() . $selected . $this->getSuffix();
+        }
+        return $this->getDefault();
+    }
 
 }
